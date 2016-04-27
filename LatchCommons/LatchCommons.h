f9fe574cb3ad7798dc87bb7165ed5fef6a4c8180
trvslhlt <trvslhlt@gmail.com>
2016-04-27 11:08:28 -0400

    First commit

---
+++ LatchCommons/LatchCommons.h
@@ -0,0 +1,19 @@
+//
+//  LatchCommons.h
+//  LatchCommons
+//
+//  Created by Travis Holt on 4/27/16.
+//  Copyright © 2016 Travis Holt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LatchCommons.
+FOUNDATION_EXPORT double LatchCommonsVersionNumber;
+
+//! Project version string for LatchCommons.
+FOUNDATION_EXPORT const unsigned char LatchCommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LatchCommons/PublicHeader.h>
+
+
